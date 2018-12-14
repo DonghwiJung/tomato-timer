abd51a3f4663c6181f7754c0b9858aa4c402edd4
donghwijung <donghwijung@intakefoods.kr>
2018-12-14 16:55:24 +0900

    doing lec 11

---
+++ components/Timer/index.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, StatusBar} from 'react-native'
+import Button from "../Button";
+
+class Timer extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle={"light-content"} />
+                <View style={styles.upper}>
+                    <Text style={styles.time}>25:00</Text>
+                </View>
+                <View style={styles.lower}>
+                    <Button iconName={"play-circle"} onPress={() => alert("Button Clicked!")} />
+                    <Button iconName={"stop-circle"} onPress={() => alert("Button Clicked!")} />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "skyblue"
+    },
+    upper: {
+        flex: 2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    lower: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    time: {
+        fontSize: 120,
+        color: 'white',
+        fontWeight: "100"
+    }
+})
+
+export default Timer;
